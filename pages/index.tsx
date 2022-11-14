6b877675a121ed5cd2e256de0318bbd98d13e3a1
Nobuhide <shinei.ishikane0134@gmail.com>
2022-11-15 00:15:47 +0900

    !とりあえずコマ置いてみた!

--- pages/index.tsx
+++ pages/index.tsx
@@ -19,9 +19,11 @@ const Mainarea = styled.div`
   margin: auto;
 `
 const Border = styled.div`
+  position: relative;
   border: solid 1px #000000;
 `
 const Container = styled.div`
+  background-color: white;
   display: flex;
   position: relative;
   height: 100vh;
@@ -29,12 +31,42 @@ const Container = styled.div`
   padding: 0 0.5rem;
 `
 
+const Piece_white = styled.div`
+  width: 60px;
+  height: 60px;
+  border-radius: 50%;
+  background-color: white;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  margin: auto;
+  position: absolute;
+`
+
+const Piece_black = styled.div`
+  width: 60px;
+  height: 60px;
+  border-radius: 50%;
+  background-color: black;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  margin: auto;
+  position: absolute;
+`
+
 const HomePage: NextPage = () => {
   return (
     <Container>
       <Mainarea>
-        <Border />
-        <Border />
+        <Border>
+          <Piece_white />
+        </Border>
+        <Border>
+          <Piece_black />
+        </Border>
         <Border />
         <Border />
         <Border />
